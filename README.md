a2bdd57271a6cded6006161014306c3ef41c6153
Sarujan P <saru.contacts@gmail.com>
2023-10-04 11:57:56 +0530

    spring boot lms initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+## Title: Spring Boot LMS
+## Author: Sarujan P
+## Email: saru.contacts@gmail.com
+## Contact: +94775428041
---
+++ src/test/java/com/saru/lms/SpringBootLmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saru.lms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootLmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
