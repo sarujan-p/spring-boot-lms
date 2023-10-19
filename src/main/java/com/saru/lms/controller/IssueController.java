fc37ba165b84f0bb586b9b3f5b8743a6c62a3040
Sarujan P <saru.contacts@gmail.com>
2023-10-19 11:31:51 +0530

    controller files are added

---
+++ src/main/java/com/saru/lms/controller/IssueController.java
@@ -0,0 +1,48 @@
+package com.saru.lms.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.saru.lms.common.Constants;
+import com.saru.lms.model.Category;
+import com.saru.lms.service.CategoryService;
+import com.saru.lms.service.IssueService;
+
+@Controller
+@RequestMapping(value = "/issue")
+public class IssueController {
+
+	@Autowired
+	private IssueService issueService;
+	
+	@Autowired
+	private CategoryService categoryService;
+	
+	@ModelAttribute(name = "memberTypes")
+	public List<String> memberTypes() {
+		return Constants.MEMBER_TYPES;
+	}
+	
+	@ModelAttribute("categories")
+	public List<Category> getCategories() {
+		return categoryService.getAllBySort();
+	}
+	
+	@RequestMapping(value = {"/", "/list"}, method = RequestMethod.GET)
+	public String listIssuePage(Model model) {
+		model.addAttribute("issues", issueService.getAllUnreturned());
+		return "/issue/list";
+	}
+	
+	@RequestMapping(value = "/new", method = RequestMethod.GET)
+	public String newIssuePage(Model model) { 
+		return "/issue/form";
+	}
+	
+}
